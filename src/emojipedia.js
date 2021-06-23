ed475ea1a0d74080add3a8b985e97b9cc8bf8e85
ANKUR2304 <17MI457@nith.ac.in>
2021-06-23 11:29:41 +0530

    initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,90 @@
+const emojipedia = [
+    {
+        id: 1,
+        emoji: "😂",
+        name: "Face With Tears of Joy",
+        meaning:
+          "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015. "
+    },
+    {
+        id: 2,
+        emoji: "😍",
+        name: "Smiling Face With Heart-Eyes",
+        meaning:
+          "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck. "
+    },
+    {
+        id: 3,
+        emoji: "😋",
+        name: "Face Savouring Delicious Food",
+        meaning:
+          "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around. "
+    },
+    
+    {
+      id: 4,
+      emoji: "🤓",
+      name: "Nerd Face",
+      meaning:
+        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+        id: 5,
+        emoji: "😄",
+        name: "Smiling Face With Open Mouth And Smiling Eyes",
+        meaning:
+          "Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm."
+    },
+    {
+        id: 6,
+        emoji: "😊",
+        name: "Smiling Face With Smiling Eyes",
+        meaning:
+          "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^"
+    },
+    {
+        id: 7,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning:
+          "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 8,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning:
+          "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+        id: 9,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        meaning:
+          "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 10,
+        emoji: "😎",
+        name: "Face With Sunglasses",
+        meaning:
+          "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+    },
+    {
+          id: 11,
+          emoji: "🤗",
+          name: "Hugging Face",
+          meaning:
+            "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+    },
+    {
+          id: 12,
+          emoji: "😭",
+          name: "Loudly Crying Face",
+          meaning:
+            "Emoji is both very sad and distraught, or is dying of laughter. Tears are pouring out of the eyes like a waterfall. Represents injury, pain or defeat. Is also used ironically or something is so funny that you die of laughter. "
+    }
+  ];
+  
+  export default emojipedia;
+  
